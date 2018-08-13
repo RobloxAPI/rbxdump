af68ad18b807589da95d41c40b456d03fc794b49
Anaminus <anaminus@protonmail.com>
2018-08-13 15:36:28 -0500

    Make Codecs thread-safe.

--- codecs.go
+++ codecs.go
@@ -2,15 +2,21 @@ package rbxapi
 
 import (
 	"io"
+	"sync"
 )
 
 // Codecs holds a number of Codec values, each referred to by a unique name.
+// Codecs is safe for use by multiple goroutines.
 type Codecs struct {
-	set map[string]Codec
+	mutex sync.Mutex
+	set   map[string]Codec
 }
 
 // Codec is the interface that is implemented by types that can decode and
 // encode an API structure.
+//
+// Codecs must not retain state between calls to Decode or Encode, and must be
+// safe for use by multiple goroutines.
 type Codec interface {
 	// Decode reads from r, decoding into an API structure.
 	Decode(r io.Reader) (root Root, err error)
@@ -40,7 +46,13 @@ func (err errUnknownCodec) UnknownCodec() string {
 // Register adds a codec under the given name. Returns false if registration
 // fails, which can occur if the codec is nil, or if a codec with the name has
 // already been registered.
+//
+// Codecs that normally allow encoding and decoding to be configured should be
+// initialized with a sensible default configuration before being registered.
 func (codecs Codecs) Register(name string, codec Codec) bool {
+	codecs.mutex.Lock()
+	defer codecs.mutex.Unlock()
+
 	if _, ok := codecs.set[name]; ok {
 		return false
 	}
@@ -55,7 +67,9 @@ func (codecs Codecs) Register(name string, codec Codec) bool {
 // the given name. Returns UnknownCodecError if the name has not been
 // registered.
 func (codecs Codecs) Decode(name string, r io.Reader) (root Root, err error) {
+	codecs.mutex.Lock()
 	codec, ok := codecs.set[name]
+	codecs.mutex.Unlock()
 	if !ok {
 		err = errUnknownCodec(name)
 		return
@@ -67,7 +81,9 @@ func (codecs Codecs) Decode(name string, r io.Reader) (root Root, err error) {
 // the given name. Returns UnknownCodecError if the name has not been
 // registered.
 func (codecs Codecs) Encode(name string, w io.Writer, root Root) (err error) {
+	codecs.mutex.Lock()
 	codec, ok := codecs.set[name]
+	codecs.mutex.Unlock()
 	if !ok {
 		err = errUnknownCodec(name)
 		return
