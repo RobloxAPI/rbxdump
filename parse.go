90eac5de1b0c23af8f9c534f4676abd461293361
Anaminus <anaminus@protonmail.com>
2018-09-03 23:00:30 -0500

    Initial implementation.

---
+++ parse.go
@@ -0,0 +1,105 @@
+// The rbxdhist package is used to parse logs involving the deployment of
+// Roblox builds.
+package rbxdhist
+
+import (
+	"regexp"
+	"time"
+)
+
+// Lex processes a stream of bytes into a stream of tokens.
+func Lex(b []byte) (s Stream) {
+	// Notes about build process (as deduced by examining the log file)
+	//
+	// When a job starts (New/Revert), it writes a message to the log file
+	// with the current time (time indicates when job starts). When the job
+	// finishes (Done) or fails (Error), it writes this status to the file. A
+	// job may be canceled, in which case no status is written. The Revert
+	// message indicates which version is reverted to.
+	//
+	// If multiple jobs start at the same time, they can inadvertently write
+	// to the file out of order (occurs on 2017/1/5). This is why status
+	// messages are separated from job messages. There is also at least one
+	// status message that does not seem to match any job message.
+	//
+	// There is one case of an irregular job message being emitted
+	// (2012/6/29). Because the message occurs only once, it is not included
+	// as a standard message, instead being parsed as a raw token.
+	//
+	// Job messages have changed in style over time. To justify the complex
+	// regexp, these changes are documented below (\n: newline, \s: trailing
+	// space).
+	const parser = `` +
+		// Original style message:
+		//     New Build version-0123456789abcdef at 1/2/2006 3:04:05 PM...\s
+		//     Revert Build version-0123456789abcdef at 1/2/2006 3:04:05 PM...\s
+		// Version addition (2011/1/6):
+		//     New Build version-0123456789abcdef at 1/2/2006 3:04:05 PM, file verion: 0, 123, 1, 12345...
+		// Spelling correction (2012/6/28):
+		//     New Build version-0123456789abcdef at 1/2/2006 3:04:05 PM, file version: 0, 123, 1, 12345...
+		// Newline prefix (2012/9/6):
+		//     \nNew Build version-0123456789abcdef at 1/2/2006 3:04:05 PM, file version: 0, 123, 1, 12345...
+		//     \nRevert Build version-0123456789abcdef at 1/2/2006 3:04:05 PM...
+		`(?:(?:^|\r?\n)?(New|Revert) (\w+) (.*?) at (\d{1,2}/\d{1,2}/\d{4} \d{1,2}:\d{2}:\d{2} (?:A|P)M)(?:, file vers?ion: (\d+, \d+, \d+, \d+))?... ?)` +
+		// Status (unchanged):
+		//     Done!\n
+		//     Error!\n
+		`|(?:(Done|Error)!\r?\n)`
+
+	parse := regexp.MustCompile(parser)
+	for i := 0; i < len(b); {
+		r := parse.FindSubmatchIndex(b[i:])
+		if r[1] < 0 {
+			// EOF
+			if i < len(s) {
+				raw := Raw(b[i:])
+				s = append(s, &raw)
+			}
+			break
+		}
+		if r[0] > 0 {
+			// There is some unparsed text between known messages.
+			raw := Raw(b[i : i+r[0]])
+			s = append(s, &raw)
+		}
+		if r[2] >= 0 {
+			// Job message.
+			job := Job{
+				Action: string(b[i+r[2] : i+r[3]]),
+				Build:  string(b[i+r[4] : i+r[5]]),
+				Hash:   string(b[i+r[6] : i+r[7]]),
+			}
+			const dateLayout = "1/2/2006 3:04:05 PM"
+			var zonePST, _ = time.LoadLocation("America/Los_Angeles")
+			var err error
+			if job.Time, err = time.ParseInLocation(dateLayout, string(b[i+r[8]:i+r[9]]), zonePST); err != nil {
+				goto parseRaw
+			}
+			if r[10] >= 0 {
+				var ok bool
+				if job.Version, ok = VersionFromString(string(b[i+r[10] : i+r[11]])); !ok {
+					goto parseRaw
+				}
+			}
+			s = append(s, &job)
+		} else if r[12] >= 0 {
+			// Status message.
+			status := Status(b[i+r[12] : i+r[13]])
+			s = append(s, &status)
+		}
+		goto next
+	parseRaw:
+		// Reparse as raw text.
+		if r[0] > 0 {
+			// Append to previous parseRaw text.
+			raw := *(s[len(s)-1].(*Raw)) + Raw(b[i+r[0]:i+r[1]])
+			s[len(s)-1] = &raw
+		} else {
+			raw := Raw(b[i : i+r[0]])
+			s = append(s, &raw)
+		}
+	next:
+		i += r[1]
+	}
+	return s
+}
---
+++ stream.go
@@ -0,0 +1,262 @@
+package rbxdhist
+
+import (
+	"bytes"
+	"encoding/json"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+type Stream []Token
+
+func (s Stream) MarshalJSON() (b []byte, err error) {
+	b = append(b, []byte(`[`)...)
+	for i, token := range s {
+		if i > 0 {
+			b = append(b, ',')
+		}
+		bsub, err := token.MarshalJSON()
+		if err != nil {
+			return nil, err
+		}
+		b = append(b, bsub...)
+	}
+	b = append(b, ']')
+	return b, nil
+}
+
+func (s *Stream) UnmarshalJSON(b []byte) error {
+	type jStream []struct {
+		Type    string
+		Action  string
+		Build   string
+		Hash    string
+		Time    time.Time
+		Version Version
+		Value   string
+	}
+	var stream jStream
+	if err := json.Unmarshal(b, &stream); err != nil {
+		return err
+	}
+	for _, token := range stream {
+		switch token.Type {
+		case "Job":
+			*s = append(*s, &Job{
+				Action:  token.Action,
+				Build:   token.Build,
+				Hash:    token.Hash,
+				Time:    token.Time,
+				Version: token.Version,
+			})
+		case "Status":
+			t := Status(token.Value)
+			*s = append(*s, &t)
+		case "Raw":
+			t := Raw(token.Value)
+			*s = append(*s, &t)
+		}
+	}
+	return nil
+}
+
+type Token interface {
+	json.Marshaler
+	json.Unmarshaler
+}
+
+type Job struct {
+	Action  string
+	Build   string
+	Hash    string
+	Time    time.Time
+	Version Version
+}
+
+func (j *Job) MarshalJSON() (b []byte, err error) {
+	var buf bytes.Buffer
+	var c []byte
+	buf.WriteString(`{"Type":"Job","Action":`)
+	c, _ = json.Marshal(j.Action)
+	buf.Write(c)
+	buf.WriteString(`,"Build":`)
+	c, _ = json.Marshal(j.Build)
+	buf.Write(c)
+	buf.WriteString(`,"Hash":`)
+	c, _ = json.Marshal(j.Hash)
+	buf.Write(c)
+	buf.WriteString(`,"Time":`)
+	c, _ = j.Time.MarshalJSON()
+	buf.Write(c)
+	if !j.Version.Empty() {
+		buf.WriteString(`,"Version":`)
+		c, _ = j.Version.MarshalJSON()
+		buf.Write(c)
+	}
+	buf.WriteString(`}`)
+	return buf.Bytes(), nil
+}
+
+func (j *Job) UnmarshalJSON(b []byte) error {
+	type jJob struct {
+		Type    string
+		Action  string
+		Build   string
+		Hash    string
+		Time    time.Time
+		Version Version
+	}
+	job := jJob{}
+	if err := json.Unmarshal(b, &job); err != nil {
+		return err
+	}
+	if job.Type != "Job" {
+		return nil
+	}
+	j.Action = job.Action
+	j.Build = job.Build
+	j.Hash = job.Hash
+	j.Time = job.Time
+	j.Version = job.Version
+	return nil
+}
+
+type Version struct {
+	Major, Minor, Maint, Build int
+}
+
+func VersionFromString(s string) (v Version, ok bool) {
+	parse := regexp.MustCompile(`` +
+		`^(\d+)\.(\d+)\.(\d+)\.(\d+)$` +
+		`|^(\d+), (\d+), (\d+), (\d+)$`,
+	)
+	r := parse.FindStringSubmatch(s)
+	if r[0] != "" {
+		if r[1] != "" {
+			v.Major, _ = strconv.Atoi(r[1])
+			v.Minor, _ = strconv.Atoi(r[2])
+			v.Maint, _ = strconv.Atoi(r[3])
+			v.Build, _ = strconv.Atoi(r[4])
+			return v, true
+		} else if r[5] != "" {
+			v.Major, _ = strconv.Atoi(r[5])
+			v.Minor, _ = strconv.Atoi(r[6])
+			v.Maint, _ = strconv.Atoi(r[7])
+			v.Build, _ = strconv.Atoi(r[8])
+			return v, true
+		}
+	}
+	return v, false
+}
+
+func (v Version) Empty() bool {
+	return v.Major == 0 && v.Minor == 0 && v.Maint == 0 && v.Build == 0
+}
+
+func (a Version) Compare(b Version) int {
+	switch {
+	case a.Major < b.Major:
+		return -1
+	case a.Major > b.Major:
+		return 1
+	case a.Minor < b.Minor:
+		return -1
+	case a.Minor > b.Minor:
+		return 1
+	case a.Maint < b.Maint:
+		return -1
+	case a.Maint > b.Maint:
+		return 1
+	case a.Build < b.Build:
+		return -1
+	case a.Build > b.Build:
+		return 1
+	}
+	return 0
+}
+
+func (v Version) MarshalJSON() (b []byte, err error) {
+	b = append(b, '"')
+	b = strconv.AppendUint(b, uint64(v.Major), 10)
+	b = append(b, '.')
+	b = strconv.AppendUint(b, uint64(v.Minor), 10)
+	b = append(b, '.')
+	b = strconv.AppendUint(b, uint64(v.Maint), 10)
+	b = append(b, '.')
+	b = strconv.AppendUint(b, uint64(v.Build), 10)
+	b = append(b, '"')
+	return b, nil
+}
+
+func (v *Version) UnmarshalJSON(b []byte) error {
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+	*v, _ = VersionFromString(s)
+	return nil
+}
+
+func (v Version) String() string {
+	return strconv.Itoa(v.Major) +
+		"." + strconv.Itoa(v.Minor) +
+		"." + strconv.Itoa(v.Maint) +
+		"." + strconv.Itoa(v.Build)
+}
+
+type Status string
+
+func (s Status) MarshalJSON() (b []byte, err error) {
+	var buf bytes.Buffer
+	var c []byte
+	buf.WriteString(`{"Type":"Status","Value":`)
+	c, _ = json.Marshal(string(s))
+	buf.Write(c)
+	buf.WriteString(`}`)
+	return buf.Bytes(), nil
+}
+
+func (s *Status) UnmarshalJSON(b []byte) error {
+	type jStatus struct {
+		Type  string
+		Value string
+	}
+	status := jStatus{}
+	if err := json.Unmarshal(b, &status); err != nil {
+		return err
+	}
+	if status.Type != "Status" {
+		return nil
+	}
+	*s = Status(status.Value)
+	return nil
+}
+
+type Raw string
+
+func (r Raw) MarshalJSON() (b []byte, err error) {
+	var buf bytes.Buffer
+	var c []byte
+	buf.WriteString(`{"Type":"Raw","Value":`)
+	c, _ = json.Marshal(string(r))
+	buf.Write(c)
+	buf.WriteString(`}`)
+	return buf.Bytes(), nil
+}
+
+func (r *Raw) UnmarshalJSON(b []byte) error {
+	type jRaw struct {
+		Type  string
+		Value string
+	}
+	raw := jRaw{}
+	if err := json.Unmarshal(b, &raw); err != nil {
+		return err
+	}
+	if raw.Type != "Raw" {
+		return nil
+	}
+	*r = Raw(raw.Value)
+	return nil
+}
