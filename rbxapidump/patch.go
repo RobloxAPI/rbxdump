915e2eabf5a1067540fd3d727b79150b1d9871e6
Anaminus <anaminus@protonmail.com>
2018-09-16 10:44:21 -0500

    Support patching on all elements.

--- rbxapidump/patch.go
+++ rbxapidump/patch.go
@@ -120,157 +120,18 @@ func copyType(typ rbxapi.Type) Type {
 //
 // Patch implements the patch.Patcher interface.
 func (root *Root) Patch(actions []patch.Action) {
-	for _, action := range actions {
+	for i, action := range actions {
 		switch action := action.(type) {
 		case patch.Member:
-			amember := action.GetMember()
-			if amember == nil {
+			aclass := action.GetClass()
+			if aclass == nil {
 				continue
 			}
-			var class *Class
-			{
-				aclass := action.GetClass()
-				if aclass == nil {
-					continue
-				}
-				name := aclass.GetName()
-				for _, c := range root.Classes {
-					if c.Name == name {
-						class = c
-						break
-					}
-				}
-			}
-			if class == nil {
-				continue
-			}
-			switch action.GetType() {
-			case patch.Remove:
-				name := amember.GetName()
-				for i, member := range class.Members {
-					if member.GetName() == name {
-						copy(class.Members[i:], class.Members[i+1:])
-						class.Members[len(class.Members)-1] = nil
-						class.Members = class.Members[:len(class.Members)-1]
-						break
-					}
-				}
-			case patch.Add:
-				class.Members = append(class.Members, copyMember(amember))
-			case patch.Change:
-				var member rbxapi.Member
-				{
-					name := amember.GetName()
-					mtype := amember.GetMemberType()
-					for _, m := range class.Members {
-						if m.GetName() == name && m.GetMemberType() == mtype {
-							member = m
-							break
-						}
-					}
-				}
-				if member == nil {
-					continue
-				}
-				switch member := member.(type) {
-				case *Property:
-					switch action.GetField() {
-					case "Name":
-						if v, ok := action.GetNext().(string); ok {
-							member.Name = v
-						}
-					case "ValueType":
-						switch v := action.GetNext().(type) {
-						case rbxapi.Type:
-							member.ValueType.SetFromType(v)
-						case string:
-							member.ValueType = Type(v)
-						}
-					case "Tags":
-						if v, ok := action.GetNext().([]string); ok {
-							member.Tags = Tags(Tags(v).GetTags())
-						}
-					}
-				case *Function:
-					switch action.GetField() {
-					case "Name":
-						if v, ok := action.GetNext().(string); ok {
-							member.Name = v
-						}
-					case "ReturnType":
-						switch v := action.GetNext().(type) {
-						case rbxapi.Type:
-							member.ReturnType.SetFromType(v)
-						case string:
-							member.ReturnType = Type(v)
-						}
-					case "Parameters":
-						if v, ok := action.GetNext().([]rbxapi.Parameter); ok {
-							member.Parameters = make([]Parameter, len(v))
-							for i, param := range v {
-								member.Parameters[i].Type.SetFromType(param.GetType())
-								member.Parameters[i].Name = param.GetName()
-								if d, ok := param.GetDefault(); ok {
-									member.Parameters[i].Default = &d
-								}
-							}
-						}
-					case "Tags":
-						if v, ok := action.GetNext().([]string); ok {
-							member.Tags = Tags(Tags(v).GetTags())
-						}
-					}
-				case *Event:
-					switch action.GetField() {
-					case "Name":
-						if v, ok := action.GetNext().(string); ok {
-							member.Name = v
-						}
-					case "Parameters":
-						if v, ok := action.GetNext().([]rbxapi.Parameter); ok {
-							member.Parameters = make([]Parameter, len(v))
-							for i, param := range v {
-								member.Parameters[i].Type.SetFromType(param.GetType())
-								member.Parameters[i].Name = param.GetName()
-								if d, ok := param.GetDefault(); ok {
-									member.Parameters[i].Default = &d
-								}
-							}
-						}
-					case "Tags":
-						if v, ok := action.GetNext().([]string); ok {
-							member.Tags = Tags(Tags(v).GetTags())
-						}
-					}
-				case *Callback:
-					switch action.GetField() {
-					case "Name":
-						if v, ok := action.GetNext().(string); ok {
-							member.Name = v
-						}
-					case "ReturnType":
-						switch v := action.GetNext().(type) {
-						case rbxapi.Type:
-							member.ReturnType.SetFromType(v)
-						case string:
-							member.ReturnType = Type(v)
-						}
-					case "Parameters":
-						if v, ok := action.GetNext().([]rbxapi.Parameter); ok {
-							member.Parameters = make([]Parameter, len(v))
-							for i, param := range v {
-								member.Parameters[i].Type.SetFromType(param.GetType())
-								member.Parameters[i].Name = param.GetName()
-								if d, ok := param.GetDefault(); ok {
-									member.Parameters[i].Default = &d
-								}
-							}
-						}
-					case "Tags":
-						if v, ok := action.GetNext().([]string); ok {
-							member.Tags = Tags(Tags(v).GetTags())
-						}
-					}
+			name := aclass.GetName()
+			for _, class := range root.Classes {
+				if class.Name == name {
+					class.Patch(actions[i : i+1])
+					break
 				}
 			}
 		case patch.Class:
@@ -292,96 +153,24 @@ func (root *Root) Patch(actions []patch.Action) {
 			case patch.Add:
 				root.Classes = append(root.Classes, copyClass(aclass))
 			case patch.Change:
-				var class *Class
-				{
-					name := aclass.GetName()
-					for _, c := range root.Classes {
-						if c.Name == name {
-							class = c
-							break
-						}
-					}
-				}
-				if class == nil {
-					continue
-				}
-				switch action.GetField() {
-				case "Name":
-					if v, ok := action.GetNext().(string); ok {
-						class.Name = v
-					}
-				case "Superclass":
-					if v, ok := action.GetNext().(string); ok {
-						class.Superclass = v
-					}
-				case "Tags":
-					if v, ok := action.GetNext().([]string); ok {
-						class.Tags = Tags(Tags(v).GetTags())
+				name := aclass.GetName()
+				for _, class := range root.Classes {
+					if class.Name == name {
+						class.Patch(actions[i : i+1])
+						break
 					}
 				}
 			}
 		case patch.EnumItem:
-			aitem := action.GetEnumItem()
-			if aitem == nil {
+			aenum := action.GetEnum()
+			if aenum == nil {
 				continue
 			}
-			var enum *Enum
-			{
-				aenum := action.GetEnum()
-				if aenum == nil {
-					continue
-				}
-				name := aenum.GetName()
-				for _, e := range root.Enums {
-					if e.Name == name {
-						enum = e
-						break
-					}
-				}
-			}
-			if enum == nil {
-				continue
-			}
-			switch action.GetType() {
-			case patch.Remove:
-				name := aitem.GetName()
-				for i, item := range enum.EnumItems {
-					if item.GetName() == name {
-						copy(enum.EnumItems[i:], enum.EnumItems[i+1:])
-						enum.EnumItems[len(enum.EnumItems)-1] = nil
-						enum.EnumItems = enum.EnumItems[:len(enum.EnumItems)-1]
-						break
-					}
-				}
-			case patch.Add:
-				enum.EnumItems = append(enum.EnumItems, copyEnumItem(aitem))
-			case patch.Change:
-				var item *EnumItem
-				{
-					name := aitem.GetName()
-					for _, i := range enum.EnumItems {
-						if i.GetName() == name {
-							item = i
-							break
-						}
-					}
-				}
-				if item == nil {
-					continue
-				}
-				switch action.GetField() {
-				case "Name":
-					if v, ok := action.GetNext().(string); ok {
-						item.Name = v
-					}
-				case "Value":
-					if v, ok := action.GetNext().(int); ok {
-						item.Value = v
-					}
-				case "Tags":
-					if v, ok := action.GetNext().([]string); ok {
-						item.Tags = Tags(Tags(v).GetTags())
-					}
+			name := aenum.GetName()
+			for _, enum := range root.Enums {
+				if enum.Name == name {
+					enum.Patch(actions[i : i+1])
+					break
 				}
 			}
 		case patch.Enum:
@@ -403,29 +192,264 @@ func (root *Root) Patch(actions []patch.Action) {
 			case patch.Add:
 				root.Enums = append(root.Enums, copyEnum(aenum))
 			case patch.Change:
-				var enum *Enum
-				{
-					name := aenum.GetName()
-					for _, e := range root.Enums {
-						if e.Name == name {
-							enum = e
-							break
+				name := aenum.GetName()
+				for _, enum := range root.Enums {
+					if enum.Name == name {
+						enum.Patch(actions[i : i+1])
+						break
+					}
+				}
+			}
+		}
+	}
+}
+
+func (class *Class) Patch(actions []patch.Action) {
+	for i, action := range actions {
+		switch action := action.(type) {
+		case patch.Member:
+			amember := action.GetMember()
+			if amember == nil {
+				continue
+			}
+			switch action.GetType() {
+			case patch.Remove:
+				name := amember.GetName()
+				for i, member := range class.Members {
+					if member.GetName() == name {
+						copy(class.Members[i:], class.Members[i+1:])
+						class.Members[len(class.Members)-1] = nil
+						class.Members = class.Members[:len(class.Members)-1]
+						break
+					}
+				}
+			case patch.Add:
+				class.Members = append(class.Members, copyMember(amember))
+			case patch.Change:
+				name := amember.GetName()
+				mtype := amember.GetMemberType()
+				for _, member := range class.Members {
+					if member.GetName() == name && member.GetMemberType() == mtype {
+						if member, ok := member.(patch.Patcher); ok {
+							member.Patch(actions[i : i+1])
 						}
+						break
 					}
 				}
-				if enum == nil {
-					continue
+			}
+		case patch.Class:
+			if action.GetType() != patch.Change {
+				continue
+			}
+			switch action.GetField() {
+			case "Name":
+				if v, ok := action.GetNext().(string); ok {
+					class.Name = v
 				}
-				switch action.GetField() {
-				case "Name":
-					if v, ok := action.GetNext().(string); ok {
-						enum.Name = v
-					}
-				case "Tags":
-					if v, ok := action.GetNext().([]string); ok {
-						enum.Tags = Tags(Tags(v).GetTags())
+			case "Superclass":
+				if v, ok := action.GetNext().(string); ok {
+					class.Superclass = v
+				}
+			case "Tags":
+				if v, ok := action.GetNext().([]string); ok {
+					class.Tags = Tags(Tags(v).GetTags())
+				}
+			}
+		}
+	}
+}
+
+func (member *Property) Patch(actions []patch.Action) {
+	for _, action := range actions {
+		if action.GetType() != patch.Change {
+			continue
+		}
+		switch action.GetField() {
+		case "Name":
+			if v, ok := action.GetNext().(string); ok {
+				member.Name = v
+			}
+		case "ValueType":
+			switch v := action.GetNext().(type) {
+			case rbxapi.Type:
+				member.ValueType.SetFromType(v)
+			case string:
+				member.ValueType = Type(v)
+			}
+		case "Tags":
+			if v, ok := action.GetNext().([]string); ok {
+				member.Tags = Tags(Tags(v).GetTags())
+			}
+		}
+	}
+}
+
+func (member *Function) Patch(actions []patch.Action) {
+	for _, action := range actions {
+		if action.GetType() != patch.Change {
+			continue
+		}
+		switch action.GetField() {
+		case "Name":
+			if v, ok := action.GetNext().(string); ok {
+				member.Name = v
+			}
+		case "ReturnType":
+			switch v := action.GetNext().(type) {
+			case rbxapi.Type:
+				member.ReturnType.SetFromType(v)
+			case string:
+				member.ReturnType = Type(v)
+			}
+		case "Parameters":
+			if v, ok := action.GetNext().([]rbxapi.Parameter); ok {
+				member.Parameters = make([]Parameter, len(v))
+				for i, param := range v {
+					member.Parameters[i].Type.SetFromType(param.GetType())
+					member.Parameters[i].Name = param.GetName()
+					if d, ok := param.GetDefault(); ok {
+						member.Parameters[i].Default = &d
 					}
 				}
+			}
+		case "Tags":
+			if v, ok := action.GetNext().([]string); ok {
+				member.Tags = Tags(Tags(v).GetTags())
+			}
+		}
+	}
+}
+
+func (member *Event) Patch(actions []patch.Action) {
+	for _, action := range actions {
+		if action.GetType() != patch.Change {
+			continue
+		}
+		switch action.GetField() {
+		case "Name":
+			if v, ok := action.GetNext().(string); ok {
+				member.Name = v
+			}
+		case "Parameters":
+			if v, ok := action.GetNext().([]rbxapi.Parameter); ok {
+				member.Parameters = make([]Parameter, len(v))
+				for i, param := range v {
+					member.Parameters[i].Type.SetFromType(param.GetType())
+					member.Parameters[i].Name = param.GetName()
+					if d, ok := param.GetDefault(); ok {
+						member.Parameters[i].Default = &d
+					}
+				}
+			}
+		case "Tags":
+			if v, ok := action.GetNext().([]string); ok {
+				member.Tags = Tags(Tags(v).GetTags())
+			}
+		}
+	}
+}
+
+func (member *Callback) Patch(actions []patch.Action) {
+	for _, action := range actions {
+		if action.GetType() != patch.Change {
+			continue
+		}
+		switch action.GetField() {
+		case "Name":
+			if v, ok := action.GetNext().(string); ok {
+				member.Name = v
+			}
+		case "ReturnType":
+			switch v := action.GetNext().(type) {
+			case rbxapi.Type:
+				member.ReturnType.SetFromType(v)
+			case string:
+				member.ReturnType = Type(v)
+			}
+		case "Parameters":
+			if v, ok := action.GetNext().([]rbxapi.Parameter); ok {
+				member.Parameters = make([]Parameter, len(v))
+				for i, param := range v {
+					member.Parameters[i].Type.SetFromType(param.GetType())
+					member.Parameters[i].Name = param.GetName()
+					if d, ok := param.GetDefault(); ok {
+						member.Parameters[i].Default = &d
+					}
+				}
+			}
+		case "Tags":
+			if v, ok := action.GetNext().([]string); ok {
+				member.Tags = Tags(Tags(v).GetTags())
+			}
+		}
+	}
+}
+
+func (enum *Enum) Patch(actions []patch.Action) {
+	for i, action := range actions {
+		switch action := action.(type) {
+		case patch.EnumItem:
+			aitem := action.GetEnumItem()
+			if aitem == nil {
+				continue
+			}
+			switch action.GetType() {
+			case patch.Remove:
+				name := aitem.GetName()
+				for i, item := range enum.EnumItems {
+					if item.GetName() == name {
+						copy(enum.EnumItems[i:], enum.EnumItems[i+1:])
+						enum.EnumItems[len(enum.EnumItems)-1] = nil
+						enum.EnumItems = enum.EnumItems[:len(enum.EnumItems)-1]
+						break
+					}
+				}
+			case patch.Add:
+				enum.EnumItems = append(enum.EnumItems, copyEnumItem(aitem))
+			case patch.Change:
+				name := aitem.GetName()
+				for _, item := range enum.EnumItems {
+					if item.GetName() == name {
+						item.Patch(actions[i : i+1])
+						break
+					}
+				}
+			}
+		case patch.Enum:
+			if action.GetType() != patch.Change {
+				continue
+			}
+			switch action.GetField() {
+			case "Name":
+				if v, ok := action.GetNext().(string); ok {
+					enum.Name = v
+				}
+			case "Tags":
+				if v, ok := action.GetNext().([]string); ok {
+					enum.Tags = Tags(Tags(v).GetTags())
+				}
+			}
+		}
+	}
+}
+
+func (item *EnumItem) Patch(actions []patch.Action) {
+	for _, action := range actions {
+		if action.GetType() != patch.Change {
+			continue
+		}
+		switch action.GetField() {
+		case "Name":
+			if v, ok := action.GetNext().(string); ok {
+				item.Name = v
+			}
+		case "Value":
+			if v, ok := action.GetNext().(int); ok {
+				item.Value = v
+			}
+		case "Tags":
+			if v, ok := action.GetNext().([]string); ok {
+				item.Tags = Tags(Tags(v).GetTags())
 			}
 		}
 	}
